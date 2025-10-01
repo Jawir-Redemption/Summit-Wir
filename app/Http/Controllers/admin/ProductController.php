c4735fc94f65835030ffe04ba8a095f349dff161
Arxvzy <szaryabima@gmail.com>
2025-10-02 00:17:02 +0700

    perf: eager load

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -54,6 +54,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
+        $product->load('category');
         return view('admin.products.show', compact('product'));
     }
 
