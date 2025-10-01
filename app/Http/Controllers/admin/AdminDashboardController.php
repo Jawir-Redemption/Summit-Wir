624bf35867a25e9acbdf5c5200acbb495bb4f411
Arxvzy <szaryabima@gmail.com>
2025-10-01 20:00:04 +0700

    refactor: add more stats for dashboard

--- app/Http/Controllers/admin/AdminDashboardController.php
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
@@ -10,10 +11,15 @@ class AdminDashboardController extends Controller
     public function index()
     {
         $totalOrders = Order::count();
+        $totalUsers = Order::count();
+        $totalProducts = Product::count();
         $successOrders = Order::where('status', 'success')->count();
         $pendingOrders = Order::where('status', 'pending')->count();
         $failedOrders = Order::where('status', 'failed')->count();
-        $latestOrders = Order::latest()->take(10)->get();
-        return view('admin.index', compact('totalOrders', 'successOrders', 'pendingOrders', 'failedOrders', 'latestOrders'));
+        $revenue = Order::whereIn('status', ['success', 'paid'])
+                ->sum('total_price');
+        $latestOrders = Order::with(['orderDetails', 'user'])->latest()->take(10)->get();
+        
+        return view('admin.index', compact('totalOrders', 'totalProducts', 'successOrders', 'pendingOrders', 'failedOrders', 'latestOrders', 'totalUsers', 'revenue'));
     }
 }
