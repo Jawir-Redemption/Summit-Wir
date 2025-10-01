e3964ff76386e7f26009fb9065206ef076bb6721
Arxvzy <szaryabima@gmail.com>
2025-10-01 20:07:54 +0700

    refactor: add route for categories

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminProductController;
 use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\CategoryController;
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminDashboardController::class, 'index'])->name('index');
@@ -11,4 +12,9 @@ Route::prefix('admin')->name('admin.')->group(function () {
      * Product Management
      */
     Route::resource('products', AdminProductController::class);
+
+    /**
+     * Category Management
+     */
+    Route::resource('categories', CategoryController::class)->except(['show']);
 });
