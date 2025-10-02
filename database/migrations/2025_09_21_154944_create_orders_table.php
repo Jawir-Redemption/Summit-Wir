db007ae0d4d8cb96535c076d5e7c60a6b670d820
Arxvzy <szaryabima@gmail.com>
2025-10-02 20:33:43 +0700

    fix: reference error

--- database/migrations/2025_09_21_154944_create_orders_table.php
+++ database/migrations/2025_09_21_154944_create_orders_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->unsignedBigInteger('orderDetail_id')->nullable();
             $table->date('loan_date');
             $table->date('return_date');
             $table->integer('duration');
@@ -24,7 +23,6 @@ return new class extends Migration
             $table->string('status')->nullable();
             $table->string('note')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
-            $table->foreign('orderDetail_id')->references('id')->on('order_details')->onDelete('set null');
             $table->timestamps();
         });
     }
