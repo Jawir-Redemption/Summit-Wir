7fc952d88f3145a0eb114d68a1be7e192b0804a3
Arya Bima <szaryabima@gmail.com>
2025-10-08 17:13:20 +0700

    refactor: create user seeder for test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,21 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin Test',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('12345678'),
+            'no_hp' => '081200000000',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Customer Test',
+            'email' => 'customer@example.com',
+            'password' => Hash::make('12345678'),
+            'no_hp' => '081200000000',
+            'role' => 'customer',
+            'email_verified_at' => now(),
         ]);
     }
 }
