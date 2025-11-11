bc96bf01f884600ddaa672c77a925abb1ee31e87
Arya Bima <szaryabima@gmail.com>
2025-11-11 07:04:13 +0700

    style: bye bye darkmode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,55 +1,43 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>@yield('title')</title>
+<html x-data="data()" lang="en">
 
-    <!-- Fonts & Styles -->
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="/assets/css/tailwind.output.css" />
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>@yield('title')</title>
 
-    <!-- Alpine.js -->
-    <script
-      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
-      defer
-    ></script>
+        <!-- Fonts & Styles -->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+            rel="stylesheet" />
+        <link rel="stylesheet" href="/assets/css/tailwind.output.css" />
 
-    <!-- Custom Scripts -->
-    <script src="/assets/js/init-alpine.js"></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
-    />
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
-      defer
-    ></script>
-    <script src="/assets/js/charts-lines.js" defer></script>
-    <script src="/assets/js/charts-pie.js" defer></script>
-  </head>
+        <!-- Alpine.js -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
-  <body>
-    <div
-      class="flex h-screen bg-gray-50 dark:bg-gray-900"
-      :class="{ 'overflow-hidden': isSideMenuOpen }"
-    >
-      <!-- Desktop sidebar -->
-      @include('components.desktop-sidebar')
+        <!-- Custom Scripts -->
+        <script src="/assets/js/init-alpine.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+        <script src="/assets/js/charts-lines.js" defer></script>
+        <script src="/assets/js/charts-pie.js" defer></script>
+    </head>
 
-      <!-- Mobile sidebar -->
-      @include('components.mobile-sidebar')
+    <body>
+        <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+            <!-- Desktop sidebar -->
+            @include('components.desktop-sidebar')
 
-      <!-- Main Content -->
-      <div class="flex flex-col flex-1 w-full">
-        @include('components.header')
-        <main class="h-full overflow-y-auto">
-          @yield('content')
-        </main>
-      </div>
-    </div>
-  </body>
+            <!-- Mobile sidebar -->
+            @include('components.mobile-sidebar')
+
+            <!-- Main Content -->
+            <div class="flex flex-col flex-1 w-full">
+                @include('components.header')
+                <main class="h-full overflow-y-auto">
+                    @yield('content')
+                </main>
+            </div>
+        </div>
+    </body>
+
 </html>
