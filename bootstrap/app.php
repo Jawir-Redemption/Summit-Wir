bbfd172993c8998306ff274f31e16ecc86c5241c
Arya Bima <szaryabima@gmail.com>
2025-11-30 13:52:08 +0700

    refactor: change path storage, change payment id, cleanup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__ . '/../routes/web.php', commands: __DIR__ . '/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [SecurityHeaders::class]);
+        $middleware->web(append: []);
 
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
--- resources/views/customer/payment.blade.php
+++ resources/views/customer/payment.blade.php
@@ -17,8 +17,7 @@
         </form>
     </section>
 
-    <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}">
-    </script>
+    <script src="/assets/js/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
 
     <script>
         document.getElementById('pay-button').addEventListener('click', function() {
